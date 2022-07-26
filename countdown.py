f4dcc446187f5be73ccf61845059719cc5e9f59c
Dilyana Koleva <64352725+littlepippilongstocking@users.noreply.github.com>
2022-07-26 09:40:32 +0100

    Fixed an error

--- countdown.py
+++ countdown.py
@@ -9,7 +9,7 @@ import time
 def countdown(t):
     while t:
         mins, secs, = divmod(t, 60)
-        timer = '{:02d}:{02d}'.format(mins, secs)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end="\r")
         time.sleep(1)
         t -= 1
